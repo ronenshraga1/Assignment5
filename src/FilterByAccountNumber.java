daf12d3b7f91ce7126da9610d5ba0109624e4734
ronenshraga1 <ronenshraga01@gmail.com>
2025-01-24 00:03:23 +0200

    mission up to 3.5

--- src/FilterByAccountNumber.java
+++ src/FilterByAccountNumber.java
@@ -1,11 +1,18 @@
 
 public class FilterByAccountNumber implements Filter<BankAccount> {
-    
+        private int minAccountNum;
+        private int maxAccountNum;
     public FilterByAccountNumber(int minAccountNum, int maxAccountNum) {
-        throw new UnsupportedOperationException("Remove this line");
+        this.minAccountNum = minAccountNum;
+        this.maxAccountNum = maxAccountNum;
     }
 
     public boolean accept(BankAccount element) {
-        throw new UnsupportedOperationException("Remove this line");
+        if(element.getAccountNumber() >= minAccountNum && element.getAccountNumber() <= maxAccountNum){
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
