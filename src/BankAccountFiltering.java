1efbbc532d0da4b99dbbf3c7e9e414f57a8a6904
ronenshraga1 <ronenshraga01@gmail.com>
2025-01-24 13:19:39 +0200

    mission up to 3.7

--- src/BankAccountFiltering.java
+++ src/BankAccountFiltering.java
@@ -1,10 +1,22 @@
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class BankAccountFiltering {
     public static List<BankAccount> getAllValidAccounts(Iterable<BankAccount> accounts, 
                                                         int requiredBalance, 
                                                         int minAccountNumber, 
                                                         int maxAccountNumber) {
-        throw new UnsupportedOperationException("Remove this line");
+        FilterByAccountNumber filterByAccountNumber = new FilterByAccountNumber(minAccountNumber,maxAccountNumber);
+        FilterByBalance filterByBalance = new FilterByBalance(requiredBalance);
+        List<Filter> filterList = new LinkedList<>();
+        filterList.add(filterByBalance);
+        filterList.add(filterByAccountNumber);
+        FilterIterator<BankAccount> filterIterator = new FilterIterator<>(accounts,(Iterable)filterList);
+        List<BankAccount> returnedlist = new LinkedList<>();
+        while (filterIterator.hasNext()){
+            returnedlist.add(filterIterator.next());
+        }
+        return returnedlist;
     }
 }
